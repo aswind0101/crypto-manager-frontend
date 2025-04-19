3a8c7c410fad4ed4819987fe751d7dc33447f7a9
Hien Vo <aswind0101@gmail.com>
2025-04-18 20:51:03 -0700

    Thay đổi nút add lên tren bảng nợ_8

--- pages/expenses.js
+++ pages/expenses.js
@@ -233,6 +233,12 @@ function Expenses() {
                             </option>
                         ))}
                     </select>
+                    {/* Mũi tên chỉ dropdown */}
+                    <div className="pointer-events-none absolute right-2 top-1/2 -translate-y-1/2">
+                        <svg className="w-4 h-4 text-black" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+                        </svg>
+                    </div>
                 </div>
 
                 {/* Nút Add nằm bên phải */}
