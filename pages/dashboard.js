0a55ea6499d403662aced7b18424a556d82b8354
Hien Vo <aswind0101@gmail.com>
2025-04-21 10:03:05 -0700

    Test giao diện mới _21

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -458,7 +458,7 @@ function Dashboard() {
     }
 
     return (
-        <div className="p-0 bg-gradient-to-br from-[#0a0f1c] via-[#050b18] to-[#020510] min-h-screen text-white">
+        <div className="p-0  max-w-[1400px] mx-auto min-h-screen text-white bg-gradient-to-br from-[#0a0f1c] via-[#050b18] to-[#020510]">
             <Navbar />
 
             <div className="mt-4 grid grid-cols-1 gap-2 p-4 rounded-xl shadow-lg bg-gradient-to-br from-[#0a0f1c] via-[#050b18] to-[#020510]">
