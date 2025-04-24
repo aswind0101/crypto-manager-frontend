56d71a6b04dc77123fd2c6ec99a53a8263c177da
Hien Vo <aswind0101@gmail.com>
2025-04-23 22:19:46 -0700

    Home:Giao diện nâu_15

--- pages/home.js
+++ pages/home.js
@@ -475,7 +475,7 @@ function Dashboard() {
                                 <select
                                     value={tradeType}
                                     onChange={(e) => setTradeType(e.target.value)}
-                                    className="bg-[#1C1F26] border border-gray-800 text-white rounded-full px-4 py-2 w-full outline-none appearance-none transition"
+                                    className="bg-[#1C1F26] border border-gray-800 text-white rounded-xl px-4 py-2 w-full outline-none appearance-none transition"
                                 >
                                     <option value="buy">Buy</option>
                                     <option value="sell">Sell</option>
@@ -489,7 +489,7 @@ function Dashboard() {
                                     value={quantity}
                                     onChange={(e) => setQuantity(e.target.value)}
                                     placeholder="e.g., 100"
-                                    className="w-full px-4 py-2 border border-gray-800 rounded-full text-white outline-none transition"
+                                    className="w-full px-4 py-2 border border-gray-800 rounded-xl text-white outline-none transition"
                                     step="any"
                                 />
                             </div>
@@ -501,7 +501,7 @@ function Dashboard() {
                                     value={price}
                                     onChange={(e) => setPrice(e.target.value)}
                                     placeholder="e.g., 2.5"
-                                    className="w-full px-4 py-2 border border-gray-800 rounded-full text-white outline-none"
+                                    className="w-full px-4 py-2 border border-gray-800 rounded-xl text-white outline-none"
                                     step="any"
                                 />
                             </div>
@@ -603,7 +603,7 @@ function Dashboard() {
                         </div>
 
                         {/* Danh sách top coin */}
-                        <div className="bg-[#1C1F26] divide-y divide-white/5 p-4">
+                        <div className="bg-gradient-to-br from-[#2f374a] via-[#1C1F26] to-[#0b0f17] divide-y divide-white/5 p-4">
                             {topCoins.slice(0, 10).map((coin) => (
                                 <div key={coin.id} className="flex items-center justify-between rounded-lg px-6 py-4 transition-all">
                                     <div className="flex items-center gap-3">
@@ -674,7 +674,7 @@ function Dashboard() {
                             : coin.profit_loss > 0 ? "∞%" : "0%";
                         return (
                             <div key={index}
-                                className="bg-[#1C1F26]  rounded-xl p-4 shadow-[2px_2px_4px_#0b0f17,_-2px_-2px_4px_#1e2631] transition-all hover:scale-[1.01]"
+                                className="bg-gradient-to-br from-[#2f374a] via-[#1C1F26] to-[#0b0f17]  rounded-xl p-4 shadow-[2px_2px_4px_#0b0f17,_-2px_-2px_4px_#1e2631] transition-all hover:scale-[1.01]"
                             >
                                 {/* Hint for mobile users */}
                                 <div className="text-center text-xs text-gray-500 italic mb-2">
