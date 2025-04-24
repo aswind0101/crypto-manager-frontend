9915e345263205c74a723e08cd0d1790d6bc6eb5
Hien Vo <aswind0101@gmail.com>
2025-04-24 14:09:39 -0700

    Home:Giao diện nâu_17

--- pages/expenses.js
+++ pages/expenses.js
@@ -271,14 +271,14 @@ function Expenses() {
             </div>
             {/* Select year + Add button */}
             <div className="max-w-4xl mx-auto mt-8 rounded-t-2xl bg-yellow-700 px-6 py-3 flex items-center justify-between 
-            shadow-md text-black text-sm font-semibold">
+            shadow-md text-white text-sm font-semibold">
                 {/* Hiển thị "Year:" và dropdown chọn năm bên trái */}
                 <div className="flex items-center gap-2 relative">
                     <span>Year:</span>
                     <select
                         value={selectedYear}
                         onChange={(e) => setSelectedYear(parseInt(e.target.value))}
-                        className="text-black font-semibold outline-none appearance-none cursor-pointer pr-6"
+                        className="text-wwhite font-semibold outline-none appearance-none cursor-pointer pr-6"
                     >
                         {availableYears.map((year) => (
                             <option key={year} value={year}>
@@ -289,7 +289,7 @@ function Expenses() {
 
                     {/* Mũi tên chỉ dropdown */}
                     <div className="pointer-events-none absolute right-2 top-1/2 -translate-y-1/2">
-                        <svg className="w-4 h-4 text-black" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+                        <svg className="w-4 h-4 text-white" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
                         </svg>
                     </div>
