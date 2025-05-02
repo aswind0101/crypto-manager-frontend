d3127e59117a1daf75303fb4e4c3fad003801d78
Hien Vo <aswind0101@gmail.com>
2025-05-01 21:54:08 -0700

    Cải thiện home:Thêm icon cảm xúc cho lời lỗ_4

--- pages/home.js
+++ pages/home.js
@@ -935,7 +935,7 @@ function Dashboard() {
                                                 <span>Update Transactions</span>
                                             </button>
 
-                                            <div className="absolute top-3 right-3">
+                                            <div className="absolute top-3 left-3">
                                                 <select
                                                     className="bg-white/5 text-yellow-300 text-xs rounded-full px-2 py-1
                                                     shadow-[2px_2px_4px_#0b0f17,_-2px_-2px_4px_#1e2631]"
