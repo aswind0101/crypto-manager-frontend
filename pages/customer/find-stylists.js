bd76f27e86c2bfc03a912884bb0085fbfed6d8a7
Hien Vo <aswind0101@gmail.com>
2025-06-15 12:45:57 -0700

    find-stylists/UI_7

--- pages/customer/find-stylists.js
+++ pages/customer/find-stylists.js
@@ -881,19 +881,19 @@ export default function FindStylists() {
                             {Array.isArray(s.services) && s.services.length > 0 && (
                               <div className="mt-2 w-full">
                                 <p className="text-[13px] font-semibold text-emerald-300 mb-1 text-left pl-2">✨ Services:</p>
-                                <div className="scroll-services-mobile px-1 py-1 bg-white/5 rounded-md text-[11px] text-pink-100 flex flex-wrap gap-2">
+                                <div className="max-h-[92px] overflow-y-auto px-1 py-1 bg-white/5 rounded-md text-[11px] text-pink-100 
+                                scroll-services-mobile grid grid-cols-2 gap-2">
                                   {s.services.map((srv) => (
                                     <div
                                       key={srv.id}
-                                      className="w-[48%] bg-white/5 rounded-lg px-2 py-[3px] capitalize"
+                                      className="bg-white/5 rounded-lg px-2 py-[3px] flex flex-col justify-between h-full"
                                     >
-                                      💅 {srv.name}
+                                      <span className="truncate block capitalize">💅 {srv.name}</span>
                                     </div>
                                   ))}
                                 </div>
                               </div>
                             )}
-
                           </div>
                           <div className="text-sm text-white/80 space-y-1">
                             <p className="text-yellow-300 font-semibold">{s.salon_name}</p>
